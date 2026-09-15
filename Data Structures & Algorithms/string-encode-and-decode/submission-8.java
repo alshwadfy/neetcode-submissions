c99a8c4676970b12c0e091ab52618c8286af5be2
Ezz Al-Shwadfy <doctorezz24@gmail.com>
2026-09-16 00:16:51 +0300

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.java
@@ -0,0 +1,30 @@
+
+
+class Solution {
+
+    public String encode(List<String> strs) {
+
+        String str ="";
+
+        for(String word : strs){
+            int length = word.length();
+             str =str+ new String(length+"#"+word);
+        }
+        return str;
+    }
+
+    public List<String> decode(String str) {
+        int i  =0;
+        List<String> res = new ArrayList<>();
+        while (i<str.length()){
+            int j = str.indexOf('#', i);
+            int len = Integer.parseInt(str.substring(i,j));
+            String word = str.substring(j+1,j+1+len);
+            res.add(word);
+            i= 1+j+len;
+
+        }
+        return res;
+
+    }
+}
